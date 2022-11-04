f28067b3c4c3d21fadcc92a4eff418ba72adb16a
Ilia Ustimov <ilia_ustimov@epam.com>
2022-11-04 17:56:14 +0400

    Nothing

--- src/index.js
+++ src/index.js
@@ -3,15 +3,29 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { DataParser } from './modules/dataParser';
+import { AuctionsFilter } from "./modules/auctionsHandler";
+import { AHEngine } from "./modules/auctionsHandler/engine";
+import { AuthToken } from './modules/dataParser/authtoken';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+class Main {
+    static init() {
+        AHEngine.init();
+    }
+}
+
+Main.init();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
